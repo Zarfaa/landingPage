1d87676346effb78cacd8183fc69eb55676a67aa
zarfaa <zarfamasood924@gmail.com>
2024-05-09 22:30:35 +0500

    feat : animation added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,14 @@
 import "./App.css";
-import logo from "./assests/desi dude logo.png"
+import logo from "./assests/logo.png"
 
 function App() {
   return (
-    <main className="grid min-h-full place-items-center px-6 py-24 sm:py-12 min-[320px]:px-6 min-[320px]:text-sm lg:px-8 mt-5 ">
-      <div className="text-center">
-        <img src={logo} alt="logo"/>
-        <div id="flip">
-            <div><div className="mt-4 text-3xl font-bold tracking-tight text_color sm:text-5xl">Launching </div></div>
-            <div><div className="mt-4 text-3xl font-bold tracking-tight text_color sm:text-5xl">This Eid-Ul-Adha</div></div>
-            <div><div className="mt-4 text-3xl font-bold tracking-tight text_color sm:text-5xl">Stay Tuned</div></div>
-          </div>
+    <main className="grid min-h-full place-items-center px-6 py-24 min-[320px]:py-10 sm:py-12 min-[320px]:px-6 min-[320px]:text-sm lg:px-8 mt-40 ">
+      <div className="text-center animation">
+        <img src={logo} alt="logo" className="w-48 mx-m-64 min-[320px]:mx-14 min-[320px]:w-40"/>
+            <div className="mt-4 text-3xl font-bold tracking-tight text_color sm:text-5xl min-[320px]:mt-10">Launching Soon</div>
+            <p className="mt-4 text-m  text_color sm:text-sm italic">Pre Bookings will start from 17th May 2024</p>
+            <div className="mt-4 text-xl font-bold tracking-tight text_color sm:text-5xl">Stay Tuned</div>
       </div>
     </main>
   );
