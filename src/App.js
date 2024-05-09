9c6e6ee761b0c7e887b3a0173d5bfe6dd44ecd1a
zarfaa <zarfamasood924@gmail.com>
2024-05-09 18:40:15 +0500

    feat: style updated

--- src/App.js
+++ src/App.js
@@ -6,11 +6,11 @@ function App() {
     <main className="grid min-h-full place-items-center px-6 py-24 sm:py-12 min-[320px]:px-6 min-[320px]:text-sm lg:px-8 mt-5 ">
       <div className="text-center">
         <img src={logo} alt="logo"/>
-        <h1 className="mt-4 text-3xl font-bold tracking-tight text_color sm:text-5xl">Launching </h1>
-        <p className="mt-6 text-base leading-7 text-gray-600">Stay Tuned</p>
-        <div className="mt-10 flex items-center justify-center gap-x-6">
-          
-        </div>
+        <div id="flip">
+            <div><div className="mt-4 text-3xl font-bold tracking-tight text_color sm:text-5xl">Launching </div></div>
+            <div><div className="mt-4 text-3xl font-bold tracking-tight text_color sm:text-5xl">This Eid-Ul-Adha</div></div>
+            <div><div className="mt-4 text-3xl font-bold tracking-tight text_color sm:text-5xl">Stay Tuned</div></div>
+          </div>
       </div>
     </main>
   );
